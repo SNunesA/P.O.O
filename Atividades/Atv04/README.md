99ef62c55fcb1210459df1d9227dd589740a3ac5
SNunesA <snunesadiers@gmail.com>
2023-04-26 19:33:40 -0300

    Atv04

--- Atividades/Atv04/README.md
+++ Atividades/Atv04/README.md
@@ -1,5 +1,12 @@
 # Atv 04
+Primeiro inicializei as variaveis com seus respectivos valores, cada uma separada em uma unica linha;
 
+E após isso apresentei a soma delas na variavel gastosTrimestre;
 
 ~~~java
+double gastosJaneiro=30000;
+double gastosFevereiro=33030.77;
+double gastosMarco=23899.01;
+double gastosTrimestre=gastosJaneiro+gastosFevereiro+gastosMarco;
+System.out.println("Gastos Trimestral:"+gastosTrimestre);
 ~~~
--- Atividades/test.java
+++ Atividades/test.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 public class test {
     public static void main(String args []){
-        // 1 - Declare uma variável do tipo real chamada gastosJaneiro e inicialize-a com o valor gasto no mês de janeiro, declare também as variáveis gastosFevereiro e gastosMarco, inicializando-as com os respectivos gastos, utilize uma linha para cada declaração;  2 - Crie uma variável chamada gastosTrimestre e inicialize-a com a soma das outras variáveis. Exiba a variável gastosTrimestre
+        
         Scanner in = new Scanner(System.in);
+        
         
        }
 }
