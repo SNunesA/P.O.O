f1cfa3382275757298f03c31b620af34d9c4c09e
SNunesA <snunesadiers@gmail.com>
2023-04-26 19:52:24 -0300

    Atv05

--- Atividades/Atv05/README.md
+++ Atividades/Atv05/README.md
@@ -1,5 +1,14 @@
 # Atv 05
+Utilizei da base da Atv 04 e complementei com a média mensal que é divida por 3 por se tratar de três meses;
 
+Após isso é exibido o resultado na tela com a formatação de duas casas decimais e o %n para pular linha.
 
 ~~~java
+double gastosJaneiro=30000;
+double gastosFevereiro=33030.77;
+double gastosMarco=23899.01;
+double gastosTrimestre=gastosJaneiro+gastosFevereiro+gastosMarco;
+System.out.println("Gastos Trimestral:"+gastosTrimestre);
+double mediaMensal=gastosTrimestre/3;
+System.out.printf("Valor da média mensal = %.2f %n",mediaMensal);
 ~~~
--- Atividades/test.java
+++ Atividades/test.java
@@ -3,9 +3,15 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 public class test {
     public static void main(String args []){
-        
+        // Declare a variável mediaMensal e faça o cálculo para a média mensal de gastos Exiba a mensagem "Valor da média mensal = " juntamente com o valor da média mensal
         Scanner in = new Scanner(System.in);
-        
+        double gastosJaneiro=30000;
+        double gastosFevereiro=33030.77;
+        double gastosMarco=23899.01;
+        double gastosTrimestre=gastosJaneiro+gastosFevereiro+gastosMarco;
+        System.out.println("Gastos Trimestral:"+gastosTrimestre);
+        double mediaMensal=gastosTrimestre/3;
+        System.out.printf("Valor da média mensal = %.2f %n",mediaMensal);
         
        }
 }
