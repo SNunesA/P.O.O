7f35b0b17eef1b4c2b9a29ec78770d3257c442dc
SNunesA <snunesadiers@gmail.com>
2023-04-26 20:30:39 -0300

    atv06

--- Atividades/test.java
+++ Atividades/test.java
@@ -3,15 +3,10 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 public class test {
     public static void main(String args []){
-        // Declare a variável mediaMensal e faça o cálculo para a média mensal de gastos Exiba a mensagem "Valor da média mensal = " juntamente com o valor da média mensal
+        // Dados os valores de um depósito fixo mensal e um montante desejado, exiba abaixo o trecho do código em java para determinar quantos meses serão necessários para acumular o montante desejado, considerando juros mensais de 0,5%.
         Scanner in = new Scanner(System.in);
-        double gastosJaneiro=30000;
-        double gastosFevereiro=33030.77;
-        double gastosMarco=23899.01;
-        double gastosTrimestre=gastosJaneiro+gastosFevereiro+gastosMarco;
-        System.out.println("Gastos Trimestral:"+gastosTrimestre);
-        double mediaMensal=gastosTrimestre/3;
-        System.out.printf("Valor da média mensal = %.2f %n",mediaMensal);
+        double deposito, montante,juros=0.5;
+        int meses;
         
        }
 }
