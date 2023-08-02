ea4d98a7724926a51d6099116f2e3904b8dc9d33
SNunesA <snunesadiers@gmail.com>
2023-08-02 19:49:01 -0300

    Atv10

--- Atividades/Atv10/ExercicioSemRetorno.java
+++ Atividades/Atv10/ExercicioSemRetorno.java
@@ -1,10 +1,24 @@
 package Atv10;
 
 public class ExercicioSemRetorno {
-    public void imprimirNumeros(){
-
+    public static void imprimirNumeros(int[] lista){
+        for(int i=0;i<lista.length;i++){
+            System.out.print(lista[i]+" ");
+        }
+        System.out.println(" ");
     }
-    public void somaNumeros(){
-
+    public static void somaNumeros(int[] lista){
+        int soma=0;
+        for(int i=0;i<lista.length;i++){
+            soma+=lista[i];
+        }
+        System.out.println("Soma:"+soma);
+    }
+    public static void main(String[] args) {
+        int [] numeros={15,26,35,47,54};
+        imprimirNumeros(numeros);
+        somaNumeros(numeros);
+        imprimirNumeros(numeros);
     }
 }
+
--- Atividades/Atv10/TestaExercicio.java
+++
@@ -1,7 +0,0 @@
-package Atv10;
-
-public class TestaExercicio {
-    public static void main(String[] args) {
-        
-    }
-}
